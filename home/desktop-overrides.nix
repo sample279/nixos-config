4a360d496f9bb295b4513518921515e5c67f8748
sample279 <224307167+sample279@users.noreply.github.com>
2026-07-16 01:13:05 +0530

    localsend override

--- home/desktop-overrides.nix
+++ home/desktop-overrides.nix
@@ -4,4 +4,11 @@
     name = "Emacs (Client)";
     noDisplay = true;
   };
+
+  xdg.desktopEntries.localsend = {
+    name = "LocalSend";
+    exec = "env GTK_CSD=0 localsend_app";
+    terminal = false;
+    categories = [ "Network" ];
+  };
 }
