5b35834037872c0a06b7f63adebc42d9f9be22c2
sample279 <224307167+sample279@users.noreply.github.com>
2026-07-17 00:47:22 +0530

    edit emacs overrides

--- home/desktop-overrides.nix
+++ home/desktop-overrides.nix
@@ -1,6 +1,23 @@
 { ... }:
 {
-  xdg.desktopEntries.emacsclient = {
+  xdg.desktopEntries.emacs = {
+      name = "Emacs";
+      genericName = "Text Editor";
+      comment = "Edit text";
+
+      exec = "emacsclient -c -a \"\" %F";
+
+      terminal = false;
+      type = "Application";
+      icon = "emacs";
+
+      categories = [ "Development" "TextEditor" ];
+
+      startupNotify = true;
+      startupWMClass = "Emacs";
+    };
+
+    xdg.desktopEntries.emacsclient = {
     name = "Emacs (Client)";
     noDisplay = true;
   };
