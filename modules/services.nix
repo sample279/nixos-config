1e8de3664b591b171cd8112bf71a6b4728d69ddf
sample279 <224307167+sample279@users.noreply.github.com>
2026-07-17 00:35:50 +0530

    enable emacs service

--- modules/services.nix
+++ modules/services.nix
@@ -5,4 +5,10 @@
   services.fstrim.enable = true;
   services.dbus.enable = true;
   services.accounts-daemon.enable = true;
+
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs30-pgtk;
+    startWithGraphical = true;
+  };
 }
